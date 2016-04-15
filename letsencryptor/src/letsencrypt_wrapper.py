7450876c2e719f156c5204a7df2b4f175a0291b8
Jan Korger <jan.korger@egym.de>
2016-04-15 13:27:14 +0200

    Adds letsencryptor service
    Sets http port to 8080. Removes nginx.

--- letsencryptor/src/letsencrypt_wrapper.py
+++ letsencryptor/src/letsencrypt_wrapper.py
@@ -1,5 +1,7 @@
 import subprocess
 
+LETSENCRYPT_PORT = 8080
+
 class LetsEncrypt(object):
     def __init__(self):
         self.letsencrypt_bin = "letsencrypt"
@@ -13,6 +15,7 @@ class LetsEncrypt(object):
             "--email=\"{}\".format(EMAIL)",
             "--standalone",
             "--standalone-supported-challenges=http-01",
+            "--http-01-port", LETSENCRYPT_PORT,
             "--rsa-key-size=2048",
             "--keep-until-expiring",
             "--domains={}".format(host_list),
